ee436f2923e388b366d086086c22948f55cf6417
Nick-Velardi <nick@domfloors.com>
2022-05-14 23:35:44 -0400

    Going to bed. Still trying to figure out how to return the playRound function.

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
 "use strict";
 const computerPlay = function () {
+  let result;
   let computerNumber = Math.random();
   if (computerNumber < 0.33) {
-    computerNumber = "PAPER";
+    result = "PAPER";
   } else if (computerNumber < 0.66) {
-    computerNumber = "ROCK";
+    result = "ROCK";
   } else if (computerNumber < 0.99) {
-    computerNumber = "SCISSORS";
+    result = "SCISSORS";
   }
-  return computerNumber;
+  return result;
 };
+
+const result = computerPlay();
 
 let playerSelection = prompt(`Rock, Paper or Scissors?`).toUpperCase();
 
-const playRound = function (computerPlay, playerSelection) {
-  if (computerPlay == "ROCK" && playerSelection == "SCISSORS") {
+const playRound = function (result, playerSelection) {
+  if (result == "ROCK" && playerSelection == "SCISSORS") {
     console.log("Rock beats scissors! You lose.");
-  } else if (computerPlay == "SCISSORS" && playerSelection == "PAPER") {
+  } else if (result == "SCISSORS" && playerSelection == "PAPER") {
     console.log("Scissors beats paper! You lose.");
-  } else if (computerPlay == "PAPER" && playerSelection == "ROCK") {
+  } else if (result == "PAPER" && playerSelection == "ROCK") {
     console.log("Paper beats rock! You lose.");
-  } else console.log(`${playerSelection} beats ${computerPlay}! You win!`);
+    //   } else if (
+    //     playerSelection !== "ROCK" ||
+    //     playerSelection !== "PAPER" ||
+    //     playerSelection !== "SCISSORS"
+    //   ) {
+    //     console.log("Not a valid command. Please select rock, paper or scissors.");
+  } else if (result == playerSelection) {
+    console.log("Tie!");
+  } else console.log(`${playerSelection} beats ${result}! You win!`);
 };
 
-playRound(computerPlay, playerSelection);
+console.log(playRound(result, playerSelection));
+
+
+const playRound = function (result, playerSelection) {
+    let winner;
+
+    if (result == "ROCK" && playerSelection == "SCISSORS") {
+      console.log("Rock beats scissors! You lose.");
+    } else if (result == "SCISSORS" && playerSelection == "PAPER") {
+      console.log("Scissors beats paper! You lose.");
+    } else if (result == "PAPER" && playerSelection == "ROCK") {
+      console.log("Paper beats rock! You lose.");
+      //   } else if (
+      //     playerSelection !== "ROCK" ||
+      //     playerSelection !== "PAPER" ||
+      //     playerSelection !== "SCISSORS"
+      //   ) {
+      //     console.log("Not a valid command. Please select rock, paper or scissors.");
+    } else if (result == playerSelection) {
+      console.log("Tie!");
+    } else console.log(`${playerSelection} beats ${result}! You win!`);
